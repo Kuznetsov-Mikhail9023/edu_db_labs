079452eb62d0a9ac40b32c27ddfd1a17d4f0488a
Kuznetsov Mikhaylo <misha.kuznetsov25@gmail.com>
2025-05-21 17:50:39 +0300

    com1

---
+++ src/sql/structure.sql
@@ -0,0 +1,9 @@
+CREATE TABLE course (
+    id BINARY(16) PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    description TEXT,
+    teacher_id BINARY(16),
+    group_id BINARY(16)
+);
+
+/* UUID зберігаються у вигляді `BINARY(16)` для компактності.
